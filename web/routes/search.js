e9155e5cf05ca48e2933e02e69785c94b1ef4d0a
andy <andy@laundrosaur.com>
2012-04-04 22:32:43 -0700

    more for search

--- web/routes/search.js
+++ web/routes/search.js
@@ -36,17 +36,27 @@ exports.card_search_partial = function( req, res, next ){
 
     var results;
     var query;
+    var query_page_size = 16;
+    var page_num = 1;
 
     if( req.params.query ) {
         console.log( req.params );
         query = encodeURIComponent( req.params.query );
+        if( req.params.page_num ){
+            page_num = req.params.page_num;
+        }
+        query_start = ( page_num - 1 ) * query_page_size;
     } else if ( req.body.query ) {
-        console.log( req.body );
         query = encodeURIComponent( req.body.query );
     }
 
     var solr_query = 
         'select?wt=json&fl=id,name,set_name,set_shortname,rules_text,name,card_type,mana,cmc,rarity,image_name,score&q=partial_name:' + query;
+
+    solr_query += '&rows=' + query_page_size;
+    solr_query += '&start=' + query_start;
+
+    console.log( solr_query );
 
     solr_client.get(solr_query, function(solr_err, solr_res) {
         if (solr_err) throw solr_err;
@@ -54,7 +64,13 @@ exports.card_search_partial = function( req, res, next ){
 
         results = solr_res_obj.response;
 
+        var num_found = results.numFound;
+
+        req.page_num = page_num;
         req.results = results.docs;
+        req.page_ceil = Math.ceil( num_found / query_page_size );
+        req.query = req.params.query;
+        req.num_found = results.numFound;
 
         next();
 
@@ -69,6 +85,10 @@ exports.results = function(req, res){
     res.render( 'search_results', { 
         "title": 'card search results',
         "results": req.results,
+        "query": req.query,
+        "page_num": req.page_num,
+        "page_ceil": req.page_ceil,
+        "num_found": req.num_found,
     });
 };
 
