a2beb8036d96454d23bbca977d3216af07c0dc77
andy <andy@laundrosaur.com>
2012-04-10 19:44:15 -0700

    archetyping put into analyzation

--- analyze/card_relation.pl
+++ analyze/card_relation.pl
@@ -16,17 +16,20 @@ use Data::Dumper;
 
 my $dbh = DBI->connect( 'dbi:Pg:dbname=decklist', 'postgres', '', { AutoCommit => 1, RaiseError => 1 } );
 
-my $unanalyzed_deck_res = $dbh->selectall_arrayref( "SELECT d.id, e.event_type_id FROM decks d JOIN events_players ep ON( d.id = ep.deck_id ) JOIN events e ON( e.id = ep.event_id ) WHERE ( d.analyzed = 'f' OR d.analyzed IS NULL ) LIMIT 1000", { Slice => {} } );
+my $unanalyzed_deck_res = $dbh->selectall_arrayref( "SELECT d.id, e.event_type_id FROM decks d JOIN events_players ep ON( d.id = ep.deck_id ) JOIN events e ON( e.id = ep.event_id ) WHERE ( d.analyzed = 'f' OR d.analyzed IS NULL ) ORDER BY e.date DESC LIMIT 100", { Slice => {} } );
 
 foreach my $deck ( @{$unanalyzed_deck_res} ) {
 
     my $deck_id = $deck->{'id'};
     my $event_type_id = $deck->{'event_type_id'};
 
-    print "Analyzing deck id $deck_id ..\n";
+    print "Analyzing deck id $deck_id of event type id $event_type_id ..\n";
+    # for debugging in-work issues currently
+    my $update_deck_state = $dbh->prepare( "UPDATE decks SET analyzed = null WHERE id = ?" );
+    $update_deck_state->execute( $deck_id );
 
     my $cards_res = $dbh->selectall_arrayref(
-        "SELECT dc.*, c.name FROM decks_cards dc JOIN cards c ON( c.id = dc.card_id ) WHERE deck_id = ? AND c.type NOT LIKE '%Land%' AND dc.type = 1",
+        "SELECT dc.*, c.name FROM decks_cards dc JOIN cards c ON( c.id = dc.card_id ) WHERE deck_id = ? AND c.type NOT LIKE '%Basic Land%' AND dc.type = 1",
         { Slice => {} },
         $deck_id
     );
@@ -105,16 +108,48 @@ foreach my $deck ( @{$unanalyzed_deck_res} ) {
 
 
     # check for archetypes
-    # get archetypes
-    # foo = 
+
+    # get archetypes 
+    my $archetypes_res = $dbh->selectall_arrayref( 
+        "SELECT ac.* FROM archetypes_cards ac JOIN archetypes a ON( ac.archetype_id = a.id ) WHERE event_type_id = ?", { Slice => {} }, $event_type_id 
+    ); 
+
+    # empty array for pushing archetype matches 
+    my @matched_archetypes;
+
     for my $card ( @{$cards_res} ) {
-        print Dumper( $card );
-        die;
+    
+        for my $archetype_card_index ( keys @{$archetypes_res} ){
+
+            if( !defined( @{$archetypes_res}[ $archetype_card_index ] ) ){
+                # card already removed from array
+                next; 
+            }
+
+            if( $card->{'card_id'} == @{$archetypes_res}[ $archetype_card_index ]->{'card_id'} ){
+
+                my $matched_card = splice( @{$archetypes_res}, $archetype_card_index, 1 );
+                push @matched_archetypes, $matched_card->{'archetype_id'};
+
+            }
+
+        }
+
     }
 
+    # count matched cards by archetype id 
+    my %archetype_match_count;
+    my %h;
+    $h{$_}++ for @matched_archetypes; 
+    my $archetype_id = (reverse sort keys %h)[0];
 
-    my $update_deck_state = $dbh->prepare( "UPDATE decks SET analyzed = 't' WHERE id = ?" );
+    if( defined $archetype_id ){
+        print "\tdeck id '$deck_id' matches archetype id '$archetype_id'\n";
+        my $update_archetype = $dbh->prepare( "UPDATE decks SET archetype_id = ? WHERE id = ?" );
+        $update_archetype->execute( $archetype_id, $deck_id );
+    }
+
+    $update_deck_state = $dbh->prepare( "UPDATE decks SET analyzed = 't' WHERE id = ?" );
     $update_deck_state->execute( $deck_id );
-
 
 }
