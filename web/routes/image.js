4e82d8d59e286fc3e25b3a9b576edba68e61df60
Stranger <stranger@laundrosaur.com>
2012-04-21 06:53:48 -0400

    addition of most of the image stuff, working with a hacked up flot module

--- web/routes/image.js
+++ web/routes/image.js
@@ -2,17 +2,43 @@
 /*
  *   IMAGE MAGIC ( ACTUAL MAGIC )
  */
-function createRandomData()
-{
-    for (var i = 10, data = []; --i;)
-        data.push([i, parseInt(Math.random() * 30)]);
+exports.transform_archetype = function(req, res, next){
+	req.plot_data = [ req.archetype_usage_plot_data ];
+	req.plot_options = { 
+		width: 450, height: 200,
+		legend: { backgroundOpacity: .5, position: 'ne', show: true },
+		xaxis: { mode: 'time', minTickSize: [ 1, "day"], labelWidth: 50, labelHeight: 20, show: true },
+		yaxis: { min: 0, max: 100, show: true },
+		grid: {	
+			backgroundColor: 'white', 
+			canvasText: { show: true, font: "sans 9px" }, 
+			legend: { position: 'ne', backgroundColor: 'white', backgroundOpacity: 0.50 },
+		},
+	};
 
-    return data;
+	next();
 }
 
-exports.image_test = function(req, res){
-
-    require('canvas');
+exports.transform_card = function(req, res, next){
+	req.plot_data = [];
+	for( key in req.daily_usage_stats ){
+		if( key == 'Standard' || key == 'Modern' || key == 'Pauper' )
+			req.plot_data.push( req.daily_usage_stats[key] );
+	}
+	req.plot_options = { 
+		width: 450, height: 200,
+		legend: { backgroundOpacity: .5, position: 'ne', show: true },
+		xaxis: { mode: 'time', minTickSize: [ 1, "day"], labelWidth: 50, labelHeight: 20, show: true },
+		yaxis: { min: 0, max: 100, show: true },
+		grid: {	
+			backgroundColor: 'white', 
+			canvasText: { show: true, font: "sans 9px" }, 
+			legend: { position: 'ne', backgroundColor: 'white', backgroundOpacity: 0.50 },
+		},
+	};
+	next();
+}
+exports.image = function(req, res){
 
     require('jsdom').env({
         html: '<html><body></body></html>', // URL or markup required
@@ -20,7 +46,8 @@ exports.image_test = function(req, res){
             // can't use jQuery 1.7+, atm, b/c of https://github.com/NV/CSSOM/issues/29
             'http://code.jquery.com/jquery-1.6.4.min.js',
             // Flot 0.7 patched to support node-canvas
-            'jquery.flot-on-node.js'
+            //'frontendjs/jquery.flot-on-node.js'
+			'frontendjs/jquery.flot.js'
         ],
         done: function (errors, window)
         {
@@ -34,40 +61,36 @@ exports.image_test = function(req, res){
             // differences from typical flot usage
             // jQuery (loaded via jsdom) can't determine element dimensions, so:
             // width and height options are required
-            var options = { width: 600, height: 300 };
-            // we can just use a stub jQuery object
+            var options = req.plot_options;
+			// we can just use a stub jQuery object
             var $placeholder = $('div');
+			$placeholder.css('width',450);
+			$placeholder.css('height',200);
+			$placeholder.css('font-size','13px');
 
             // Flot data format and options are unchanged
-            var data = [{
-                data: createRandomData(),
-                lines: {show: true, fill: true}
-            }, {
-                data: createRandomData(),
-                lines: {show: false},
-                bars: {show: true}
-            }, {
-                data: createRandomData(),
-                lines: {show: true},
-                points: {show: true}
-            }];
-
-            console.log( 'attempting to plot' );
+            var data = req.plot_data;
 
             // call Flot as usual
-            var $plot = $.plot($placeholder, data, options)
+            var $plot = $.plot( $placeholder, data, options )
+
+			var ctx = $plot.getCanvas().getContext("2d");
+			ctx.fillStyle = '#000';
+			ctx.font = 'bold 10px sans-serif';
+			//ctx.textBaseline( 'bottom' );
+			ctx.fillText( 'Generated @ mtgotech.com', 35, 25 );
 
             // get the node-canvas instance
             var nodeCanvas = $plot.getCanvas();
 
             // write the file
-            var fs = require('fs');
+			res.writeHead(200, {'Content-Type': 'image/png', 'Transfer-Encoding': 'chunked'} );
             nodeCanvas.toBuffer(function (error, buffer) {
                 if (error) throw error;
-                    fs.writeFile(__dirname + '/flot.png', buffer);
+				res.end( buffer );
             });
 
-            res.send( 'ok!' );
+
         }
 
 
